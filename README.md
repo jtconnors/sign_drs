6bd950b0c9772259cf4699f6f95667135ccfedb6
Jim Connors <github@jtconnors.com>
2017-12-13 22:51:05 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,157 @@
+# sign_drs
+Powershell script to facilitate signing Deployment Rule Sets with either a
+self-signed or trusted certificate
+
+
+## OVERVIEW
+
+Java SE Advanced Support customers can download and use security
+updates to older versions of Java that are not publicly available.  However,
+as of the October 2017 patch update release for Java 6 (6u171) and 7 (7u161),
+the Java plugin, necessary to run Java applets and WebStart applications, is
+no longer provided.  In order for customer's web based (applets and WebStart)
+applications to run with these latest versions, they need to configure
+Deployment Rule Sets.  More information about Deployment Rule Sets can be found
+here:
+
+https://docs.oracle.com/javase/8/docs/technotes/guides/deploy/deployment_rules.html
+
+Describing Rule Sets in detail is beyond the scope of this README, suffice it
+to say, in order to correctly Deploy a rule set, the following high-level
+setps must take place:
+
+1. Create rules (in XML syntax) and store them in a file called ```ruleset.xml```
+2. Using the ```jar(1)``` utility, create a ```DeploymentRuleSet.jar``` file that
+   includes the ```ruleset.xml``` file
+3. Sign the ```DeploymentRuleSet.jar``` file with a code-signing certificate
+4. Place the signed ```DeploymentRuleSet.jar``` in the following Windows directory:
+   ```C:\Windows\Sun\Java\Deployment\```
+   
+All four steps must be successfully completed.  Failure of any type in the
+aforementioned sequence will result in an invalid Deployment Rule Set.
+
+This project deals specifically with step 3, namely the process of signing the
+```DeploymentRuleSet.jar``` file.
+
+The powershell script contained in this repository facilitates signing the
+```DeploymentRuleSet.jar``` file -- a process considered by some to be both 
+mysterious and challenging.  It can be directed to sign the Rule Set with
+a trusted certificate  (definitely the preferred safer method), or for those
+without access to a trusted certificate, with a self-signed variety that is
+created on the fly.
+
+
+## TRYING IT OUT
+
+With no modification to the script contained in this repository, it should be
+straightforward to create a sample self-signed rule set.  Issue the following
+commands from a DOS CMD prompt in the directory containing both this
+```README.md``` file and the ```drs-sign-ruleset.ps1``` script:
+
+   ```
+   copy sample-ruleset.xml ruleset.xml  
+   powershell .\drs-sign-ruleset.ps1  
+   ```
+   
+The resulting run will produce three files:
+
+   ```
+   DeployMentRuleSet.jar  
+   drscacerts  
+   deployment.properties
+   ```
+
+Once they are copied to the C:\Windows\Sun\Java\Deployment\ directory,
+(you'll need Administrator privieges to do so), a Deployment Rule Set will
+be active.
+
+
+## SIGNING WITH A TRUSTED CERTIFICATE FROM A CERTITICATE AUTHORITY
+
+For those utilizing Deployment Rule Sets in production, it is STRONGLY
+RECOMMENDED that the ```DeploymentRuleSet.jar``` file be signed with a certificate
+from a trusted authority.  Beacuse it would be impossible to determine
+beforehand the specifics of your trusted certificate, some customization of
+the ```drs-sign-ruleset.ps1``` file is required.  Near the beginning of the script
+there is a small section dedicated to setting the properties necessary to
+use your signing certificate.  It appears as follows:
+
+  ```
+  Set-Variable -Name CA_CERT_KEYSTORE -Value "C:\MyKeystore.jks"  
+  Set-Variable -Name CA_CERT_ALIAS -Value "My Alias"  
+  Set-Variable -Name CA_CERT_KEYSTORE_PASSWD -Value "changeit"  
+  Set-Variable -Name CA_CERT_TRUSTSTORE_PASSWD -Value "changeit"  
+  Set-Variable -Name CA_CERT_TIMESTAMP_AUTHORITY -Value "http://MyTimestampURL"  
+  ```
+  
+#### Here's a brief description of the 4 (or 5 if you want to timestamp) powershell varaibles that must be set:  
+
+  ```$CA_CERT_KEYSTORE``` - This is the name of the file containing the encrypted 
+     private and public keys of your signing certificate  
+
+  ```$CA_CERT_ALIAS``` - Certificates are assigned an alias -- a string -- which is
+     a more human readable way of uniquely identifying this entry in
+     the keystore  
+ 
+  ```$CA_CERT_KEYSTORE_PASSSWD```  
+  ```$CA_CERT_TRUSTSTORE_PASSWD``` - Keystores are assigned passwords required
+     to access respectively the keystore (containing private keys) and the
+     truststore (containing the public certificates).  In many cases these
+     two passwords are one in the same.  
+
+  ```$CA_CERT_TIMESTAMP_AUTHORITY``` - This setting is required if you want to
+     timestamp your signed Deployment Rule Set.  It can be initiated by 
+     running the script with the '-timestamp' option.  The timestamp
+     authority is a URL that that is typically provided by the Vendor
+     responsible for issuing the trusted certificate.  
+
+Other than the far better security implications, a signed Deployment Rule Set
+from a trusted authority only needs to place the signed ```DeploymentRuleSet.jar```
+file in the ```C:\Windows\Sun\Java\Deployment\``` directory.  The additional
+```deployment.properties``` and ```drscacerts``` files, required for self-signed
+certificates, are not needed, and should not exist.  
+
+
+## COMMAND-LINE OPTIONS
+
+A number of command-line arguments, a certain set of which are mandatory,
+are available for running the ```drs-sign-ruleset.ps1``` script. A brief description
+of the options follows.
+
+#### One of the following options must be selected.  Failure to do so will result in the termination of the program with a non-zero exit status:
+
+  ```-selfsign```  Sign the Deployment Rule Set with a generated self-signed
+             certificate.  When complete, three files will be created:
+             (1) ```DeploymentRuleSet.jar```, (2) ```deployment.properties``` and
+             (3) ```drscaccerts```.  All three files must be placed, unmodified,
+             in the ```C:\Windows\Sun\Java\Deployment\``` directory to enable
+             the Java plugin to use the Deployment Rule Set feature.  
+
+  ```-casign```    Sign the Deployment Rule Set with a trusted certificate from a
+             certificate authority.  When complete, only one file will be
+             created: ```DeploymentRuleSet.jar```.  It must be placed, unmodified,
+             in the ```C:\Windows\Sun\Java\Deployment\``` directory to enable
+             the Java plugin to use the Deployment Rule Set feature.  
+
+  ```-help```      Prints a help screen briefly describing the command-line
+             options then exits.  
+
+  ```-clean```     Deletes all files generated by this script and returns the
+             directory to its original state.  Upon completion, exit.  
+
+#### The following arguments can be optionally be placed on the command-line
+
+  ```-timestamp``` Timestamping a signed application enables it to remain vaild
+             even after its certificate has expired.  Use this option if
+             you know the timestamp URL location of your vendor (one is
+             already provided for self-signed certificates) and you want to
+             timestamp your signed rule set.  
+
+  ```-jks```       Recently the default keystore for the Java ```keytool(1)``` utility
+             has transitioned to the more modern, less proprietary PKCS12
+             format.  If you wish to generate the keystore and trustsore in
+             legacy JKS format, use this option.  
+
+  -verbose   Signing a rule set is composed of multiple steps.  Using this
+             option will output each individual command, with arguments, that
+             is executed along the way.  
---
+++ drs-sign-ruleset.ps1
@@ -0,0 +1,346 @@
+﻿#
+# Powershell script to sign a Deployment Rule Set with either a self-signed
+# certificate or a trusted certificate from a Certificate Authority.
+#
+# Invoke this script with the '-help' option for a brief description on
+# how to run.
+#
+
+##################################################
+#
+# Variables Section: Important! Modifications may be required here,
+# and definitely will be if signing with a trusted certificate.
+#
+
+#
+# This variable MUST be set with a valid Java JDK location
+#
+Set-Variable -Name JDK_HOME -Value "C:\Program Files\Java\jdk1.8.0_152"
+
+#
+# If you want to sign the Deployment Rule Set with a trusted certificate
+# these variables MUST be set with valid values.
+#
+Set-Variable -Name CA_CERT_KEYSTORE -Value "C:\MyKeystore.jks"
+Set-Variable -Name CA_CERT_ALIAS -Value "My Alias"
+Set-Variable -Name CA_CERT_KEYSTORE_PASSWD -Value "changeit"
+Set-Variable -Name CA_CERT_TRUSTSTORE_PASSWD -Value "changeit"
+# This variable is optional and must be set if using the '-timestamp' option
+Set-Variable -Name CA_CERT_TIMESTAMP_AUTHORITY -Value "http://MyTimestampURL"
+
+#
+# These variables are used for self-signing the Deployment Rule Set.
+# They can be left as is or customized to suit your needs.
+#
+Set-Variable -Name DN -Value "CN=Ruleset Signer, OU=jtconnors.com, O=Test, L=New York, ST=NY, C=US"
+Set-Variable -Name SELF_SIGN_KEYSTORE -Value "keystore.jks"
+Set-Variable -Name SELF_SIGN_ALIAS -Value "selfsign_drs"
+Set-Variable -Name SELF_SIGN_KEYSTORE_PASSWD -Value "changeit"
+Set-Variable -Name SELF_SIGN_TRUSTSTORE_PASSWD -Value "changeit"
+Set-Variable -Name SELF_SIGN_CERT_FILE -Value "$SELF_SIGN_ALIAS.cer"
+
+#
+# These variables are best left as they are
+# 
+Set-Variable -Name SELF_SIGN_TRUSTSTORE -Value drscacerts
+Set-Variable -Name SELF_SIGN_TIMESTAMP_AUTHORITY -Value "http://sha256timestamp.ws.symantec.com/sha256/timestamp"
+
+#
+# End Variables Section
+#
+##################################################
+
+##################################################                              
+#
+# Helper Functions
+#
+
+#
+# Print a command with all its args on one line. 
+#
+function Print-Cmd {
+    Foreach ($item in $args[0]) {
+       $CMD += $item
+       $CMD += " "
+    }
+    Write-Output $CMD
+}
+
+#
+# Execute a command, optionally print out that command and check exit status.
+# If status is non-zero, exit
+#
+function Run-Cmd {
+    Set-Variable -Name OPTIONS -Value @()
+    $COMMAND = $($args[0][0])
+    Foreach ($item in $args[0][1]) {
+       $OPTIONS += $item
+    }
+    if ($VERBOSE -eq "true") {
+        Print-Cmd ($COMMAND, $OPTIONS)
+    }
+    & $COMMAND $OPTIONS
+    if ($LASTEXITCODE -ne 0) {
+        Exit $LASTEXITCODE
+    }
+}
+
+#
+# Delete files produced from previous run if they exist
+#
+function Do-Clean {
+    Foreach ($item in @("$SELF_SIGN_KEYSTORE",
+                        "$SELF_SIGN_TRUSTSTORE",
+                        "$SELF_SIGN_CERT_FILE",
+                        "DeploymentRuleSet.jar",
+                        "deployment.properties")) {
+        if ((Test-Path $item)) {
+            Remove-Item $item
+        } 
+    }
+}
+
+#
+# End Helper functions
+#
+##################################################
+
+##################################################
+#
+# Command-line argument processing
+#
+Set-Variable -Name SCRIPT_NAME -Value $MyInvocation.MyCommand.Name
+
+function Print-Help {
+    Write-Output "${SCRIPT_NAME}: Deployment RuleSet signing script"
+    Write-Output ""
+    Write-Output "One of the following options must be used:"
+    Write-Output ""
+    Write-Output " -help         Print this Message and exit"
+    Write-Output " -clean        Delete generated files and exit"
+    Write-Output " -selfsign     Sign Deployment Rule Set with self-signed certificate"
+    Write-Output " -casign       Sign Deployment Rule Set with a trusted certificate from a CA"
+    Write-Output ""
+    Write-Output "The following arguments are optional:"
+    Write-Output ""
+    Write-Output " -timestamp    Sign with a timestamp"
+    Write-Output " -jks          Use JKS keystore type instead of default PKCS12"
+    Write-Output " -verbose      Output commands used to accomplish signing"
+    Write-Output ""
+}
+
+Set-Variable -Name DO_SELF_SIGN -Value false
+Set-Variable -Name DO_CA_SIGN -Value false
+Set-Variable -Name USE_TIMESTAMP -Value false
+Set-Variable -Name USE_JKS -Value false
+
+if ($args.count -gt 0) {
+    Foreach ($arg in $args) {
+        switch ($arg) {
+            '-help' { 
+                Print-Help
+                Exit 0
+            }
+            '-clean' { 
+                Do-Clean
+                Exit 0
+            }
+            '-selfsign' { 
+                Set-Variable -Name DO_SELF_SIGN -Value true            
+                Set-Variable -Name KEYSTORE -Value $SELF_SIGN_KEYSTORE
+                Set-Variable -Name ALIAS -Value $SELF_SIGN_ALIAS
+                Set-Variable -Name KEYSTORE_PASSWD -Value $SELF_SIGN_KEYSTORE_PASSWD
+                Set-Variable -Name TRUSTSTORE_PASSWD -Value $SELF_SIGN_TRUSTSTORE_PASSWD
+                Set-Variable -Name CERT_FILE -Value $SELF_SIGN_CERT_FILE
+                Set-Variable -Name TRUSTSTORE -Value $SELF_SIGN_TRUSTSTORE
+                Set-Variable -Name TIMESTAMP_AUTHORITY -Value $SELF_SIGN_TIMESTAMP_AUTHORITY
+            }
+            '-casign' { 
+                Set-Variable -Name DO_CA_SIGN -Value true
+                Set-Variable -Name KEYSTORE -Value $CA_CERT_KEYSTORE
+                Set-Variable -Name ALIAS -Value $CA_CERT_ALIAS
+                Set-Variable -Name KEYSTORE_PASSWD -Value $CA_CERT_KEYSTORE_PASSWD
+                Set-Variable -Name TRUSTSTORE_PASSWD -Value $CA_CERT_TRUSTSTORE_PASSWD
+                Set-Variable -Name TIMESTAMP_AUTHORITY -Value $CA_CERT_TIMESTAMP_AUTHORITY
+            }
+            '-timestamp' { Set-Variable -Name USE_TIMESTAMP -Value true }
+            '-jks' { Set-Variable -Name USE_JKS -Value true }
+            '-verbose' { Set-Variable -Name VERBOSE -Value true }
+            default {
+                Write-Output "`nUnknown option: ""$arg""`n"
+                Print-Help
+                Exit 1               
+            }
+        } 
+    }
+} else {
+    Print-Help
+    Exit 1
+}
+
+if ($DO_SELF_SIGN -eq "false" -and $DO_CA_SIGN -eq "false") {
+    Print-Help
+    Exit 0
+}
+#
+# End command-line argument processing
+#
+##################################################
+
+#
+# Make sure all files/directories in the following list can be accessed
+# 
+Foreach ($item in @("$JDK_HOME",
+                     "$JDK_HOME\bin\keytool.exe",
+                     "$JDK_HOME\bin\jar.exe",
+                     "$JDK_HOME\bin\jarsigner.exe",
+                     'ruleset.xml')) {
+    if (!(Test-Path $item)) {
+        Write-Error -Message "Cannot Access $item"
+        Exit -1
+    } 
+}
+
+#
+# Delete files produced from previous run if they exist
+#
+Do-Clean
+
+#
+# Step 1: If using a self-signed certificate, generate the self-signed
+# certificate
+#
+if ($DO_SELF_SIGN -eq "true") {
+    #
+    # Step 1, part a: generate a key pair that will be used as the 
+    # "Cerificate Authority"
+    #
+    Set-Variable -Name GEN_KEYPAIR_CMD_ARGS -Value @(
+        '-genkey',
+        '-keyalg',
+        'RSA',
+        '-alias',
+        """$ALIAS""",
+        '-keystore',
+        """$KEYSTORE""",
+        '-dname',
+        """$DN""",
+        '-storepass',
+        """$TRUSTSTORE_PASSWD""",
+        '-keypass',
+        """$KEYSTORE_PASSWD""",
+        '-validity',
+        '730',
+        '-keysize',
+        '2048',
+        '-storetype'
+        )
+    if ($USE_JKS -eq "false") {
+        $GEN_KEYPAIR_CMD_ARGS += 'pkcs12'
+    } else {
+        $GEN_KEYPAIR_CMD_ARGS += 'jks'
+    }
+    Write-Output "`n*** Generating key pair in ""$KEYSTORE"" with alias ""$ALIAS""`n"   
+    Run-Cmd("$JDK_HOME\bin\keytool.exe", $GEN_KEYPAIR_CMD_ARGS)
+
+    #
+    # Step 1, part b: Export the signing certificate that was generated in
+    # the previous step
+    #
+    Set-Variable -Name EXPORT_CMD_ARGS -Value @(
+        '-export',
+        '-keystore',
+        """$KEYSTORE""",
+        '-storepass',
+        """$KEYSTORE_PASSWD""",
+        '-file',
+        """$CERT_FILE""",
+        '-alias',
+        """$ALIAS"""
+        )
+    Write-Output "*** Exporting Certificate from ""$KEYSTORE"" to ""$CERT_FILE""`n"
+    Run-Cmd("$JDK_HOME\bin\keytool.exe", $EXPORT_CMD_ARGS)
+
+    #
+    # Step 1, part c: Import the signing certificate into the "drscacerts"
+    # truststore.
+    #
+    Set-Variable -Name IMPORT_CMD_ARGS -Value @(
+        '-import',
+        '-noprompt',
+        '-keystore',
+        """$TRUSTSTORE""",
+        '-storepass',
+        """$TRUSTSTORE_PASSWD""",
+        '-file',
+        """$CERT_FILE""",
+        '-alias',
+        """$ALIAS"""
+        )
+    Write-Output "`n*** Importing Certificate from ""$CERT_FILE"" into ""$TRUSTSTORE""`n"
+    Run-Cmd("$JDK_HOME\bin\keytool.exe", $IMPORT_CMD_ARGS)
+}
+
+#
+# Step 2: Create the DeploymnentRuleSet.jar file
+#
+Set-Variable -Name JAR_ARGS -Value @(
+    'cvf',
+    'DeploymentRuleSet.jar',
+    'ruleset.xml'
+)
+Write-Output "`n*** Creating DeploymentRuleSet.jar with ruleset.xml`n"
+Run-Cmd("$JDK_HOME\bin\jar.exe", $JAR_ARGS)
+
+#
+# Step 3: Sign the DeploymentRuleSet.jar file.
+#
+Set-Variable -Name JARSIGNER_ARGS -Value @(
+    '-keystore',
+    """$KEYSTORE""",
+    '-storepass',
+    """$TRUSTSTORE_PASSWD""",
+    '-keypass',
+    """$KEYSTORE_PASSWD"""
+)
+if ($USE_TIMESTAMP -eq "true") {
+    $JARSIGNER_ARGS += '-tsa'
+    $JARSIGNER_ARGS += "$TIMESTAMP_AUTHORITY"
+    Write-Output "`n*** Signing DeploymentRuleSet.jar with timestamp authority $TIMESTAMP_AUTHORITY`n"
+} else {
+    Write-Output "`n*** Signing DeploymentRuleSet.jar without timestamp`n"
+}
+$JARSIGNER_ARGS += 'DeploymentRuleSet.jar'
+$JARSIGNER_ARGS += """$ALIAS"""
+
+Run-Cmd("$JDK_HOME\bin\jarsigner.exe", $JARSIGNER_ARGS)
+
+#
+# Step 4: If using a self-signed certificate, create the deployment.properties file
+#
+if ($DO_SELF_SIGN -eq "true") {
+    Write-Output "`n*** Creating the deployment.properties file"
+    $DATE = Get-Date
+    Add-content -Path deployment.properties -Value "# Created on $DATE"
+    Add-Content -Path deployment.properties "deployment.user.security.trusted.cacerts=C\:\\Windows\\Sun\\Java\\Deployment\\$TRUSTSTORE"
+}
+
+#
+# Output a message stating that the process is complete, which files were created and where
+# those file(s) have to be placed to enable the Deployment Rule Set functionality.
+#
+if ($DO_SELF_SIGN -eq "true") {
+    $GENERATED_FILES = "deployment.properties DeploymentRuleSet.jar drscacerts"
+} else {
+    $GENERATED_FILES = "DeploymentRuleSet.jar"
+}
+Write-Output "`n*** Signing complete."
+Write-Output ""
+Write-Output "In order to enable Deployment Rule Sets on your system, copy"
+Write-Output "the following file(s):"
+Write-Output ""
+Write-Output "    $GENERATED_FILES"
+Write-Output ""
+Write-Output "to the C:\Windows\Sun\Java\Deployment\ directory"
+Write-Output ""
+Exit 0
